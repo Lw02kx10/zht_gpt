65479a12437f06436b22185ec00b091fcbdf6106
Wen <1691637805@qq.com>
2024-01-13 11:01:25 +0800

    页面大致结构搭建

---
+++ src/components/ChatItem.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="chat-root">
+        <div class="chat-wrapper">
+            <el-icon class="chat-icon">
+                <ChatDotSquare />
+            </el-icon>
+            <div class="chat-title">
+                <span>{{ title }}</span>
+            </div>
+            <el-icon class="edit-icon">
+                <EditPen />
+            </el-icon>
+            <el-icon class="delete-icon">
+                <Delete />
+            </el-icon>
+        </div>
+    </div>
+</template>
+
+<script setup lang="js">
+import { ChatDotSquare, EditPen, Delete } from "@element-plus/icons-vue";
+defineProps(['title']);
+</script>
+
+<style lang="scss" scoped>
+$gray: #e5e7eb;
+$green: #6c6c6c;
+@mixin icon-style {
+    width: 10px;
+    display: flex;
+    align-items: center;
+}
+.chat-root {
+    margin-top: 5px;
+    .chat-wrapper {
+        display: flex;
+        height: 23px;
+        padding: 3px 0px;
+        margin-left: 10px;
+        margin-right: 10px;
+        border: 1px solid $gray;
+        font-size: 9.5px;
+        line-height: 23px;
+        border-radius: 4px;
+        cursor: pointer;
+        &:hover {
+            background-color: #f5f5f5;
+        }
+        .chat-icon {
+            @include icon-style;
+            margin-left: 6px;
+            margin-right: 6px;
+        }
+        .chat-title {
+            flex-grow: 1;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+        }
+        .edit-icon {
+            @include icon-style;
+            margin-left: 6px;
+        }
+        .delete-icon {
+            @include icon-style;
+            margin-left: 6px;
+            margin-right: 6px;
+        }
+    }
+}
+</style>
--- src/pages/MainHome.vue
+++ src/pages/MainHome.vue
@@ -1,13 +1,79 @@
 <template>
     <div class="root">
-        
+        <div class="container-wrapper">
+            <div class="chat-record">
+                <div class="add-wrapper">
+                    <div class="add-btn">
+                        <span>新增会话</span>
+                    </div>
+                </div>
+                <div class="chat-wrapper">
+                    <ChatItem title="crx的意思"/>
+                </div>
+            </div>
+            <div class="chat-context"></div>
+        </div>
     </div>
 </template>
 
 <script setup lang="js">
+import { ref, onMounted } from "vue";
+import ChatItem from "../components/ChatItem.vue";
 
+onMounted(() => {
+
+})
 </script>
 
 <style lang="scss" scoped>
-
+$gray: #e5e7eb;
+$green: #6c6c6c;
+.root {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    .container-wrapper {
+        margin: 10px;
+        flex-grow: 1;
+        border-radius: 5px;
+        box-shadow: 3px 3px 13px;
+        background-color: #fff;
+        display: flex;
+        .chat-record {
+            width: 23%;
+            border-right: 1px solid $gray;
+            border-radius: 5px 0 0 5px;
+            /* background-color: #6c6c6c; */
+            display: flex;
+            flex-direction: column;
+            .add-wrapper {
+                height: 30px;
+                .add-btn {
+                    height: 18px;
+                    padding: 3px 0;
+                    margin-top: 6px;
+                    margin-left: 10px;
+                    margin-right: 10px;
+                    border: 1px dashed $gray;
+                    line-height: 18px;
+                    text-align: center;
+                    font-size: 9.5px;
+                    cursor: pointer;
+                    transition: all 0.5s;
+                    border-radius: 3px;
+                    &:hover {
+                        border: 1px dashed $green;
+                        color: $green;
+                    }
+                }
+            }
+            .chat-wrapper {
+                margin-top: 8px;
+            }
+        }
+        .chat-context {
+            width: 77%;
+        }
+    }
+}
 </style>
